45f70c92c8261dd04c274ac5f6377435a4dfe532
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-02-28 11:17:51 +0300

    mod_rewrite

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/autoload.php';
 
+$url = $_SERVER['REQUEST_URI']; // по ключику находится строка которую ввел пользователь
+
 $controller = new \App\Controllers\News();
 
 $action = $_GET['action'] ? : 'Index';
