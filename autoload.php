90995e89d0b95875f733e0e3294a376680970386
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-03-02 07:56:54 +0300

    spl_autoload_register

--- autoload.php
+++ autoload.php
@@ -1,6 +1,22 @@
 <?php
 
-function __autoload($class)
+function myAppAutoload($class)
 {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    $filename = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+
+    if (file_exists($filename)) {
+        include $filename;
+    }
 }
+
+spl_autoload_register('myAppAutoload');
+/* Первый способ */
+
+/* Второй способ */
+spl_autoload_register(function ($class)
+{
+    $filename = __DIR__ . '/' . str_replace(['\\', 'App'], ['/', 'lib'], $class) . '.php';
+    if (file_exists($filename)) {
+        include $filename;
+    }
+});
